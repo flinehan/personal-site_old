a30bdacdfc78017c90e6e11c58111350a3d864d9
frank <frankie.lineha@gmail.com>
2015-06-28 12:09:18 -0600

    more updated layout changes
    Signed-off-by: frank <frankie.lineha@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
 /**
  * Created by Frank on 12/27/2014.
  */
-(function (window) {
-  $('a[href^="#"]').on('click', function(event) {
-      var target = $( $(this).attr('href') );
-      if( target.length ) {
-          event.preventDefault();
-          $('html, body').animate({
-              scrollTop: target.offset().top
-          }, 400);
-      }
-    });
-})(window);
-
-$( document ).ready(function() {
-    $(".button-collapse").sideNav();
-
-    var background = $('.fc-background');
-
-    var parallax = function() {
-      var scrolled = $(window).scrollTop();
-      background.css('top', -(scrolled * 0.8) + 'px');
-    };
-
-    //this is a greedy function, but we have to fire this each time
-    //so that the parallax scrolls nice :)
-    if(background){
-      $(window).scroll(function(e) {
-        parallax();
-      });
-    }
-});
-
 
